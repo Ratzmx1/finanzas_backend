85b4d06ba4d1e53ae2392d9bacf8622dbfd460c3
Varela69 <luxo_v7@hotmail.com>
2021-02-09 17:02:33 -0300

    profits

---
+++ src/models/profits.ts
@@ -0,0 +1,31 @@
+import { Schema, Document, model } from "mongoose";
+
+export interface IProfits {
+  date: Date;
+  type: string;
+  number: number;
+  products: [
+    {
+      name: string;
+      quantity: number;
+      price: number;
+    }
+  ];
+}
+
+const profitsSchema = new Schema({
+  date: Date,
+  type: String,
+  number: Number,
+  products: [
+    {
+      name: String,
+      quantity: Number,
+      price: Number,
+    },
+  ],
+});
+
+interface IProfitsExtend extends IProfits, Document {}
+
+export const Profits = model<IProfitsExtend>("profits", profitsSchema);
