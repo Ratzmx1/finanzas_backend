4c35ad5463f4fcd5d18709bdd4799018d2c90d03
Ratzmx <enzoxz.023@gmail.com>
2021-02-09 04:09:30 -0300

    Second Commit

--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,20 @@
 import express from "express";
 import mongoose from "mongoose";
+import bodyparser from "body-parser";
 
 import ExampleRouter from "./routes/exampleRoute";
 
 const app = express();
-
 const port = process.env.PORT || 8000;
-
 const dbname = process.env.DBNAME || "test";
-
 const mongooseURL = `mongodb+srv://admin:admin@cluster0.y28gj.mongodb.net/${dbname}?retryWrites=true&w=majority`;
 
 mongoose.connect(mongooseURL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
+
+app.use(bodyparser.json());
 
 app.use("/example", ExampleRouter);
 
